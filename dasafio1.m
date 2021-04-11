9a886bee19b730e298ad1a3974f105ea567a4031
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-04-11 17:52:12 -0400

    finish homework

---
+++ dasafio1.m
@@ -0,0 +1,25 @@
+% 5 calculate
+
+
+clear
+resp_5_a = (sin(7*pi/9))/(cos(5*pi/7)^2) + (1/7)*tan(5*pi/12)
+resp_5_b = tan(64)/(cos(14)^2) - (3*sin(80))/(0.9^(1/3)) + (cos(55))/(sin(11))
+
+% 6 Define the variable x as x=2.34, then evaluate
+x =2.34
+resp_6_a = 2*x^4 - 6*x^3 + 14.8*x^2 + 9.1
+resp_6_b = e^(2*x)/sqrt(14 + x^2 - x)
+
+% 7 Define the variable t as t=6.8, then evaluate
+t = 6.8
+
+resp_7_a = log(abs(t^2-t^3))
+resp_7_b = 75/(2*t) * cos(0.8*t - 3)
+
+% 8 Define the variablesx and y as x=8.3 and y=2.4, then evaluate
+x = 8.3
+y = 2.4
+
+resp_8_a = x^2 + y^2 - (x^2)/(y^2)
+resp_8_b = sqrt(x*y) - sqrt(x+y) + ((x-y)/(x-(2*y)))^2 - sqrt(x/y)
+
